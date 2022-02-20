ee2b7f1b9fa66ee4619b811d6cb747d0e737b051
gonpress <gonpress@naver.com>
2022-02-20 12:11:18 +0900

    mongoose CRUD

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -23,6 +23,20 @@ router.get('/', asyncHandler( async (req,res) => {
     });
 }));
 
+// 특정 Product를 불러오는 API
+router.get('/:id', asyncHandler( async (req, res) => {
+
+    const productId = req.params.id;
+    const product = await productModel.findById(productId);
+    if(product){
+        res.json(product);
+    }else{
+        res.status(404).json({
+            msg: '프로덕트 없음',
+        })
+    }
+}))
+
 // Product를 등록하는 API
 router.post('/', asyncHandler( async (req, res) => {
 
@@ -40,20 +54,61 @@ router.post('/', asyncHandler( async (req, res) => {
     res.json(createdProduct);
 }));
 
-
 // Product를 수정하는 API
-router.put('/', (req, res) => {
-    res.json({
-        msg: '프로덕트 수정 됨',
-    })
-})
+router.put('/:id', asyncHandler(async (req, res) => {
+    const {id} = req.params;
 
-// Product를 삭제하는 API
-router.delete('/', (req, res) => {
+    const {name, price, brand, desc} = req.body;
+
+    const product = await productModel.findById(id);
+    if(product){
+        product.name = name;
+        product.price = price;
+        product.brand = brand;
+        product.desc = desc;
+
+        const updatedProduct = await product.save();
+        res.json({
+            msg: 'updated at ' + id,
+            updatedProduct,
+        })
+
+    }else {
+        res.status(404).json({
+            msg: '수정할 데이터가 없습니다.',
+        })
+    }
+
+    /*res.json({
+        msg: '프로덕트 수정 됨',
+    })*/
+}));
+
+// Product 전체를 삭제하는 API
+router.delete('/', asyncHandler( async (req, res) => {
+    await productModel.deleteMany({});
     res.json({
-        msg: '프로덕트 삭제 됨'
+        msg: '프로덕트 삭제 됨',
     })
-})
+}));
+
+// 특정 Product를 삭제하는 API
+router.delete('/:id', asyncHandler( async (req, res) => {
+    const productId = req.params.id;
+
+    const product = await productModel.findById(productId);
+    if(product){
+        await product.remove();
+        res.json({
+            msg: '프로덕트 removed',
+        })
+    } else{
+        res.status(404).json({
+            msg: '프로덕트 없음',
+        })
+
+    }
+}))
 
 export default router;
 
