1803930b86cc775ac6d53edaedf352797d228361
gonpress <gonpress@naver.com>
2022-02-20 11:37:53 +0900

    get 요청 시 필요한 데이터만 return

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,21 +5,34 @@ import productModel from "../models/productModel.js";
 const router = express.Router();
 
 // Product를 불러오는 API
-router.get('/', (req,res) => {
+router.get('/', asyncHandler( async (req,res) => {
+
+    const products = await productModel.find();
     res.json({
-        msg: '전체 불러오기',
-        products: []
-    })
-})
+        count: products.length,
+        products: products.map(product => (
+            {
+                id: product._id,
+                name: product.name,
+                price: product.price,
+                brand: product.brand,
+                desc: product.desc,
+            }
+        ))
+
+    });
+}));
 
 // Product를 등록하는 API
 router.post('/', asyncHandler( async (req, res) => {
 
+    const {name, price, brand, description} = req.body;
+
     const newProduct = new productModel({
-        name: req.body.name,
-        price: req.body.price,
-        brand: req.body.brand,
-        desc: req.body.description,
+        name,
+        price,
+        brand,
+        desc:description,
     });
 
     const createdProduct = await newProduct.save();
