5ecd03b4d66a0c5cbeffedf30bf08cd80924f4c4
gonpress <gonpress@naver.com>
2022-02-26 11:57:10 +0900

    add user register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,53 @@
-import express from 'express'
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import userModel from "../models/userModel.js";
+import bcrypt from 'bcryptjs';
+import gravatar from 'gravatar';
+
 const router = express.Router();
 
 
-//
+// 회원가입 API
+router.post('/register', asyncHandler(async(req, res)=> {
+    const {name, email, password} = req.body;
+    const userExists = await userModel.findOne({email})
+    if(userExists) {
+        return res.status(404).json({
+            msg:'User already exists',
+        })
+    }
+
+    // password 암호화
+    const salt = await bcrypt.genSalt(10);
+    const passwordHashed = await bcrypt.hash(password, salt);
 
 
+    // 프로필 이미지 자동생성
+    const avatar = await gravatar.url(email, {
+        protocol:'https',
+        s:'200',
+        r:'pg',
+        d:'mm'
+    })
 
+    const user = new userModel({
+        name,
+        email,
+        password:passwordHashed,
+        profileImg:avatar
+    });
 
+    const createdUser = await user.save();
+    res.json({
+        msg:'유저 생성 완료',
+        userInfo:createdUser,
+    })
+}))
 
-
-
-
+// 로그인 API
+router.post('/login', asyncHandler(async(req, res) => {
+    res.json({msg:'login'});
+}))
 
 
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import connectDB from "./config/db.js";
 // Router
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-// import {productRoutes, orderRoutes} from './routes';
+import userRoutes from "./routes/userRoutes.js";
 
 // ES5 문법
 // const express = require("express")
@@ -22,14 +22,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 app.get("/", (req, res) => {
     res.send("api running");
 })
 
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
-
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 8888;
 
